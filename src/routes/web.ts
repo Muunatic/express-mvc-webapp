9486d326c942f50cbb0a82ed551f5e1a93c6f536
Muunatic <muunatic@typeslint.com>
2025-09-09 18:34:27 +0700

    feat(route): check session on login route

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { restrict } from "../middlewares/middleware";
+import { checkSession, restrict } from "../middlewares/middleware";
 import { homeView, loginView, orderView, payView, profileView, registerView } from "../controllers/baseController";
 
 const app = Router();
 
 app.get("/", homeView);
-app.get("/login", loginView);
+app.get("/login", checkSession, loginView);
 app.get("/register", registerView);
 app.get("/profile", restrict, profileView);
 app.get("/pay", restrict, payView);
